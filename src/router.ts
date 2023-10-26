5b5003643f978a7f7abd0be86ffd218068e26027
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-10-26 17:05:04 -0300

    feat: buscando filmes por id

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { createMovie } from "./controllers/moviesControllers";
+import { createMovie, findMovieById } from "./controllers/moviesControllers";
 import { validate } from "./middleware/handleValidation";
 import { movieCreateValidation } from "./middleware/movieValidation";
 
@@ -9,4 +9,5 @@ export default router
    .get("/test", (req: Request, res: Response) => {
       res.status(200).send("API Working");
    })
-   .post("/movie", movieCreateValidation(), validate, createMovie);
+   .post("/movie", movieCreateValidation(), validate, createMovie)
+   .get("/movie/:id", findMovieById);
